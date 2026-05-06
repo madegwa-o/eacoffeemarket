22d21e67157a2e8e7df919594d2d0b6433db15f6
madegwa-o <madegwaoscar317@gmail.com>
2026-05-06 18:29:23 +0300

    new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,17 @@
 
 import FintechQuizGame from "@/components/quiz-page";
 
+// export default function Home() {
+//     return (
+//         <main className="min-h-screen">
+//             <FintechQuizGame />
+//         </main>
+//     )
+// }
 export default function Home() {
     return (
         <main className="min-h-screen">
-            <FintechQuizGame />
+            <h1>I will fill the home page later</h1>
         </main>
     )
 }
